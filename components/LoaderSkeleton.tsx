008f6faacbe42afaad4d0df3126cebe3da1765bb
18charmi <12charmi@gmail.com>
2025-07-19 19:37:20 +0530

    ui: update loader skeleton

--- components/LoaderSkeleton.tsx
+++ components/LoaderSkeleton.tsx
@@ -1,11 +1,8 @@
-import { Box, Skeleton } from "@mui/material";
+import {  Skeleton } from "@mui/material";
 
 export default function LoaderSkeleton() {
-    return <Box width={'100%'}>
-        <Skeleton height={64} className="m-2 box-content" />
-        <Skeleton height={64} className="m-2 box-content" animation="wave" />
-        <Skeleton height={64} className="m-2 box-content" animation={false} />
-        <Skeleton height={64} className="m-2 box-content" animation={false} />
-        <Skeleton height={64} className="m-2 box-content" animation={false} />
-    </Box>
+    return <div className="flex justify-center items-center min-h-screen min-w-screen">
+        <Skeleton variant="circular" width={100} height={100} animation="wave" />
+        
+    </div>
 }
